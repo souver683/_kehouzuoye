58e4fa96d03ef944ce6564f4aa4e10f3a2987a9f
souver683 <73768247+souver683@users.noreply.github.com>
2021-09-20 14:38:37 +0800

    Update 希尔排序.java

--- src/希尔排序.java
+++ src/希尔排序.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
 public class 希尔排序 {
-}
+    public static void main(String[] args) {
+        int key,fnc,i,j;
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[8];
+        int temp=0;
+        for(int k=0;k<a.length;k++){
+            a[k]=sc.nextInt();
+        }
+        for(fnc = a.length / 2;fnc>0;fnc /=2){
+            for(i = fnc;i < a.length; i++){
+                for (j = i - fnc; j >= 0; j -= fnc) {
+                    if(a[j] > a[j + fnc])
+                    {temp = a[j];
+                        a[j] = a[j + fnc];
+                        a[j + fnc] = temp;}
+                }}
+        }for(int k=0;k<a.length;k++){
+            System.out.print(a[k]);
+            System.out.print(" ");}
+    }}
