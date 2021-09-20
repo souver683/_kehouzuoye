b6ff7129bdb7d07b55a9bc84d18862c7dab9497c
souver683 <73768247+souver683@users.noreply.github.com>
2021-09-20 14:37:53 +0800

    Update 冒泡排序.java

--- src/冒泡排序.java
+++ src/冒泡排序.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
 public class 冒泡排序 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[] a=new int[10];
+        for(int k=0;k<10;k++){
+            a[k]=sc.nextInt();
+        }
+        int temp=0;
+        for(int i=0;i<a.length-1;i++){
+            for(int j=0;j<a.length-i-1;j++){
+                if(a[j]>a[j+1]){
+                    temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+        for(int k=0;k<10;k++){
+            System.out.print(a[k]);
+            System.out.print(" ");
+        }
+
+    }
 }
